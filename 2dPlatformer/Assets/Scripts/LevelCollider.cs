b8663c3d4491bbf3271646ee453aa34ec8f348bd
vvr <vover@mail.ru>
2021-02-25 19:01:12 +0300

    character and tiles export

---
+++ 2dPlatformer/Assets/Scripts/LevelCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCollider : MonoBehaviour
+{
+
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            child.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
