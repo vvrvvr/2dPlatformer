c9ce15e34c79dc9297ca6967b97d510ce89b2de6
vvr <vover@mail.ru>
2021-03-03 14:43:05 +0300

    physics, colliders, pushing and another minor changes

--- 2dPlatformer/Assets/Scripts/Player.cs
+++ 2dPlatformer/Assets/Scripts/Player.cs
@@ -9,15 +9,20 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float minSpeedToRun;
     [SerializeField] private LayerMask solidLayer;
+    [SerializeField] private float groundOffset;
+    [SerializeField] private float pushOffset;
 
     private Rigidbody2D rb;
     private Animator anim;
     private CapsuleCollider2D boxCol;
     private Vector2 movement;
+    private Vector2 boundsMax;
+    private Vector2 boundsMin;
     private float deltaX;
     private bool isJump;
     private bool grounded;
     private bool isPushing;
+    private bool isAttack;
 
     void Start()
     {
@@ -26,36 +31,58 @@ public class Player : MonoBehaviour
         boxCol = GetComponent<CapsuleCollider2D>();
     }
 
-
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         isJump |= Input.GetButtonDown("Jump");
+        isAttack = Input.GetKeyDown(KeyCode.F);
+
         deltaX = horizontal * speed;
+        boundsMax = boxCol.bounds.max;
+        boundsMin = boxCol.bounds.min;
 
-        //rotate sprite
-        if (!Mathf.Approximately(deltaX, 0))
-            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        CheckGround();
+        CheckToPush(horizontal);
+        PlayerAnimation(horizontal);
+    }
 
-        //check for ground
-        Vector3 max = boxCol.bounds.max;
-        Vector3 min = boxCol.bounds.min;
-        Vector2 corner1 = new Vector2(max.x, min.y - .05f);
-        Vector2 corner2 = new Vector2(min.x, min.y - .1f);
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void CheckGround()
+    {
+        Vector2 corner1 = new Vector2(boundsMax.x - 0.025f, boundsMin.y - groundOffset);
+        Vector2 corner2 = new Vector2(boundsMin.x + 0.025f, boundsMin.y - groundOffset);
+       // Debug.DrawLine(corner1, corner2, Color.red, 0.001f, false);
         Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
         grounded = false;
         if (hit != null)
             grounded = true;
+    }
 
-        //check for something to push
-        Vector2 corner3 = new Vector2(max.x + (0.01f * Mathf.Sign(horizontal)), min.y + 0.01f);
-        Vector2 corner4 = new Vector2(min.x + (0.01f * Mathf.Sign(horizontal)), min.y + 0.01f);
+    private void CheckToPush(float horizontalAxisInput)
+    {
+        float dir = 0;
+        if (horizontalAxisInput != 0)
+            dir = Mathf.Sign(horizontalAxisInput);
+        Vector2 corner3 = new Vector2(boundsMax.x + dir * pushOffset, boundsMin.y + 0.1f);
+        Vector2 corner4 = new Vector2(boundsMin.x + dir * pushOffset, boundsMin.y + 0.1f);
+        Debug.DrawLine(corner3, corner4, Color.green, 0.001f, false);
+        Debug.Log(dir);
         Collider2D hitPush = Physics2D.OverlapArea(corner3, corner4, solidLayer);
         isPushing = false;
         if (hitPush != null)
             isPushing = true;
+        
+    }
 
-        #region Animation
+    private void PlayerAnimation(float horizontalAxisInput)
+    {
+        //rotate sprite
+        if (!Mathf.Approximately(deltaX, 0))
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
         //running
         if (Mathf.Abs(deltaX) > minSpeedToRun)
         {
@@ -64,28 +91,29 @@ public class Player : MonoBehaviour
         else
             anim.SetBool("isRunning", false);
         //jumping
-        if (Mathf.Abs(rb.velocity.y) > 0)
+        if (!grounded)
         {
             anim.SetBool("isJumping", true);
-            anim.SetBool("isAttack", false);
+            anim.ResetTrigger("Attack");
         }
         else
             anim.SetBool("isJumping", false);
         //Attack
-        if (Input.GetKeyDown(KeyCode.F) && Mathf.Abs(rb.velocity.y) == 0)
+        if (isAttack && Mathf.Abs(rb.velocity.y) == 0)
         {
-            anim.SetBool("isAttack", true);
+            anim.SetTrigger("Attack");
+            isAttack = false;
         }
         //pushing
-        if (horizontal != 0 && Mathf.Abs(rb.velocity.y) == 0 && isPushing)
+        if (horizontalAxisInput != 0 && Mathf.Abs(rb.velocity.y) == 0 && isPushing)
         {
             anim.SetBool("isPushing", true);
         }
         else
             anim.SetBool("isPushing", false);
-        #endregion
     }
-    private void FixedUpdate()
+
+    private void MoveCharacter()
     {
         movement = new Vector2(deltaX, rb.velocity.y);
         if (isJump && grounded)
@@ -98,6 +126,6 @@ public class Player : MonoBehaviour
 
     public void AttackEnd()
     {
-        anim.SetBool("isAttack", false);
+        anim.SetTrigger("Attack");
     }
 }
